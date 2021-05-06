8f7fe58b6e423d1a741b710942cdb51e85755f5b
prapatsorn-k <76081944+patsornk@users.noreply.github.com>
2021-05-06 10:14:42 +0700

    [T1PT-353] create new branch section 2 ui (#71)

--- components/organisms/create-branch/branch.vue
+++ components/organisms/create-branch/branch.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="create-branch-container">
     <span class="header">Branch Information</span>
-    <div class="input-section">
+    <div class="input-section-2">
       <input-field
         v-model="$v.brandId.$model"
         title="Select Brand"
@@ -76,6 +76,130 @@
         :errorMessage="error.partnerCodeId"
       />
     </div>
+
+    <span class="header">Location</span>
+    <div class="input-section-2">
+      <input-field
+        v-model="$v.branchTypeId.$model"
+        title="Branch Type"
+        required
+        type="select"
+        :options="branchTypeList"
+        :optionsReduce="(item) => item.branchTypeId"
+        :optionsLabel="
+          language === 'th' ? 'companyCategoryTh' : 'companyCategoryEn'
+        "
+        placeholder="Please select..."
+        :errorMessage="error.branchTypeId"
+      />
+      <input-field
+        v-model="$v.mallId.$model"
+        title="Mall"
+        required
+        type="select"
+        :options="mallList"
+        :optionsReduce="(item) => item.mallId"
+        :optionsLabel="
+          language === 'th' ? 'companyCategoryTh' : 'companyCategoryEn'
+        "
+        placeholder="Please select..."
+        :errorMessage="error.mallId"
+      />
+    </div>
+    <div class="input-section-3">
+      <input-field
+        v-model="$v.countryId.$model"
+        title="Country"
+        required
+        type="select"
+        :options="countryList"
+        :optionsReduce="(item) => item.countryId"
+        :optionsLabel="
+          language === 'th' ? 'companyCategoryTh' : 'companyCategoryEn'
+        "
+        placeholder="Please select..."
+        :errorMessage="error.countryId"
+      />
+      <input-field
+        v-model="$v.provinceId.$model"
+        title="Province"
+        required
+        type="select"
+        :options="provinceList"
+        :optionsReduce="(item) => item.provinceId"
+        :optionsLabel="
+          language === 'th' ? 'companyCategoryTh' : 'companyCategoryEn'
+        "
+        placeholder="Please select..."
+        :errorMessage="error.provinceId"
+      />
+      <input-field
+        v-model="$v.districtId.$model"
+        title="District"
+        required
+        type="select"
+        :options="districtList"
+        :optionsReduce="(item) => item.districtId"
+        :optionsLabel="
+          language === 'th' ? 'companyCategoryTh' : 'companyCategoryEn'
+        "
+        placeholder="Please select..."
+        :errorMessage="error.branchTypeId"
+      />
+      <input-field
+        v-model="$v.subDistrictId.$model"
+        title="Sub - district"
+        required
+        type="select"
+        :options="subDistrictList"
+        :optionsReduce="(item) => item.subDistrictId"
+        :optionsLabel="
+          language === 'th' ? 'companyCategoryTh' : 'companyCategoryEn'
+        "
+        placeholder="Please select..."
+        :errorMessage="error.subDistrictId"
+      />
+      <input-field
+        v-model="$v.postalCode.$model"
+        title="Postal Code"
+        :maxlength="50"
+        required
+        :errorMessage="error.postalCode"
+      />
+      <div></div>
+      <input-field
+        v-model="$v.latitude.$model"
+        title="Latitude"
+        :maxlength="50"
+        required
+        :errorMessage="error.latitude"
+      />
+      <input-field
+        v-model="$v.longitude.$model"
+        title="Longitude"
+        :maxlength="50"
+        required
+        :errorMessage="error.longitude"
+      />
+
+      <button
+        class="btn-map"
+        :class="
+          error.latitude ||
+          error.longitude ||
+          $v.latitude.$model === '' ||
+          $v.longitude.$model === ''
+            ? 'disable'
+            : ''
+        "
+        @click="findInMap"
+      >
+        Find in map
+      </button>
+    </div>
+
+    <google-map class="branch-map" :position="mapPosition" />
+
     <div class="submit-section">
       <button class="submit" @click="submit">Save</button>
     </div>
@@ -86,13 +210,16 @@
 import { Component, Vue, Watch } from 'vue-property-decorator'
 import InputField from '@/components/atoms/InputField.vue'
 import PhoneNumInput from '@/components/atoms/PhoneNumInput.vue'
+import GoogleMap from '~/components/atoms/GoogleMap.vue'
+import { MapPosition } from '@/constants/types/GoogleMapTypes.js'
 import { validationMixin } from 'vuelidate'
 import {
   required,
   minLength,
   maxLength,
   email,
-  numeric
+  numeric,
+  decimal
 } from 'vuelidate/lib/validators'
 
 const validations = {
@@ -130,6 +257,16 @@ const validations = {
     maxLength: maxLength(13)
   },
   partnerCodeId: { required },
+  branchTypeId: { required },
+  mallId: { required },
+  address: { required },
+  countryId: { required },
+  provinceId: { required },
+  districtId: { required },
+  subDistrictId: { required },
+  postalCode: { required },
+  latitude: { required, decimal },
+  longitude: { required, decimal },
 
   validationGroup: [
     'brandId',
@@ -140,7 +277,18 @@ const validations = {
     'siebelBranchName',
     'email',
     'phoneNumber',
-    'partnerCodeId'
+    'partnerCodeId',
+    'branchTypeId',
+    'mallId',
+    'address',
+
+    'countryId',
+    'provinceId',
+    'districtId',
+    'subDistrictId',
+    'postalCode',
+    'latitude',
+    'longitude'
   ]
 }
 
@@ -149,7 +297,8 @@ const validations = {
   validations: validations,
   components: {
     InputField,
-    PhoneNumInput
+    PhoneNumInput,
+    GoogleMap
   }
 })
 export default class CreateBranch extends Vue {
@@ -164,6 +313,17 @@ export default class CreateBranch extends Vue {
   email = ''
   companyPhonePrefix = '+66'
   phoneNumber = ''
+  partnerCodeId = ''
+  branchTypeId = ''
+  mallId = ''
+  address = ''
+  countryId = ''
+  provinceId = ''
+  districtId = ''
+  subDistrictId = ''
+  postalCode = ''
+  latitude = ''
+  longitude = ''
 
   private error = {
     brandId: '',
@@ -175,14 +335,35 @@ export default class CreateBranch extends Vue {
     email: '',
     companyPhonePrefix: '',
     phoneNumber: '',
-    partnerCodeId: ''
+    partnerCodeId: '',
+    branchTypeId: '',
+    mallId: '',
+    address: '',
+    countryId: '',
+    provinceId: '',
+    districtId: '',
+    subDistrictId: '',
+    postalCode: '',
+    latitude: '',
+    longitude: ''
   }
 
   private brandList = []
   private partnerCodeList = []
+  private branchTypeList = []
+  private mallList = []
+  private countryList = []
+  private provinceList = []
+  private districtList = []
+  private subDistrictList = []
 
   get language(): any {
     return this.$i18n.locale
+  }
+
+  mapPosition: MapPosition = {
+    lat: Number(13.7457381),
+    lng: Number(100.5371002)
   }
 
   @Watch('brandId')
@@ -266,8 +447,83 @@ export default class CreateBranch extends Vue {
       : ''
   }
 
+  @Watch('branchTypeId')
+  checkBranchTypeId(): void {
+    this.error.branchTypeId = !this.$v.branchTypeId.required
+      ? this.$t('createCompany.error.require').toString()
+      : ''
+  }
+
+  @Watch('mallId')
+  checkMallId(): void {
+    this.error.mallId = !this.$v.mallId.required
+      ? this.$t('createCompany.error.require').toString()
+      : ''
+  }
+
+  @Watch('countryId')
+  checkCountryId(): void {
+    this.error.countryId = !this.$v.countryId.required
+      ? this.$t('createCompany.error.require').toString()
+      : ''
+  }
+
+  @Watch('provinceId')
+  checkProvinceId(): void {
+    this.error.provinceId = !this.$v.provinceId.required
+      ? this.$t('createCompany.error.require').toString()
+      : ''
+  }
+
+  @Watch('districtId')
+  checkDistrictId(): void {
+    this.error.districtId = !this.$v.districtId.required
+      ? this.$t('createCompany.error.require').toString()
+      : ''
+  }
+
+  @Watch('subDistrictId')
+  checkSubDistrictId(): void {
+    this.error.subDistrictId = !this.$v.subDistrictId.required
+      ? this.$t('createCompany.error.require').toString()
+      : ''
+  }
+
+  @Watch('postalCode')
+  checkPostalCode(): void {
+    this.error.postalCode = !this.$v.postalCode.required
+      ? this.$t('createCompany.error.require').toString()
+      : ''
+  }
+
+  @Watch('latitude')
+  checkLatitude(): void {
+    this.error.latitude = !this.$v.latitude.required
+      ? this.$t('createCompany.error.require').toString()
+      : !this.$v.latitude.decimal
+      ? this.$t('decimal').toString()
+      : ''
+  }
+
+  @Watch('longitude')
+  checkLongitude(): void {
+    this.error.longitude = !this.$v.longitude.required
+      ? this.$t('createCompany.error.require').toString()
+      : !this.$v.longitude.decimal
+      ? this.$t('decimal').toString()
+      : ''
+  }
+
   private onChangedPrefixNumber(value: string): void {
     this.companyPhonePrefix = value
+  }
+
+  private findInMap() {
+    if (this.$v.latitude.$model && this.$v.longitude.$model) {
+      console.log('findInMap')
+      this.mapPosition.lat = Number(this.$v.latitude.$model)
+      this.mapPosition.lng = Number(this.$v.longitude.$model)
+    }
   }
 
   async getBrand(): Promise<any> {
@@ -358,6 +614,15 @@ export default class CreateBranch extends Vue {
       this.checkEmail()
       this.checkPhoneNumber()
       this.checkPartnerCodeId()
+      this.checkBranchTypeId()
+      this.checkMallId()
+      this.checkCountryId()
+      this.checkProvinceId()
+      this.checkDistrictId()
+      this.checkSubDistrictId()
+      this.checkPostalCode()
+      this.checkLatitude()
+      this.checkLongitude()
     } else {
       const payload = {
         companyNameTh: this.$v.companyNameTh.$model,
@@ -430,13 +695,14 @@ export default class CreateBranch extends Vue {
 
 .create-branch-container {
   padding-top: 60px;
+  width: 100%;
 
   .header {
     font-size: 24px;
     font-weight: 700;
   }
 
-  .input-section {
+  .input-section-2 {
     display: grid;
     grid-template-columns: auto auto;
     grid-column-gap: 60px;
@@ -446,6 +712,43 @@ export default class CreateBranch extends Vue {
     .input-field,
     .phone-num-input {
       min-width: 420px;
+    }
+  }
+
+  .input-section-3 {
+    display: grid;
+    grid-template-columns: auto auto auto;
+    grid-column-gap: 60px;
+    grid-row-gap: 25px;
+    padding: 30px 10% 30px 0px;
+
+    .input-field {
+      min-width: 260px;
+    }
+
+    .btn-map {
+      cursor: pointer;
+      justify-content: center;
+      align-items: center;
+
+      background: $mid-black;
+      color: $white;
+      border-radius: 6px;
+      box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.1);
+      width: 112px;
+      height: 36px;
+      margin-top: 35px;
+      font-size: 14px;
+      font-weight: bold;
+
+      &.disable {
+        cursor: initial;
+        background: $gray-disable;
+
+        &:focus {
+          outline: none;
+        }
+      }
     }
   }
 
@@ -470,6 +773,12 @@ export default class CreateBranch extends Vue {
       font-size: 16px;
       font-weight: bold;
     }
+  }
+
+  .branch-map {
+    width: 100%;
+    height: 329px;
+    margin-bottom: 30px;
   }
 }
 </style>
