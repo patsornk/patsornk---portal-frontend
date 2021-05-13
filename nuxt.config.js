6086f156615805bc7964881eb8d2ea4f8233a65a
Wiwat Patanaprasitchai <unknown>
2021-05-13 16:42:50 +0700

    [CHORE] Fix ci

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,12 @@ export default {
 
   mode:'spa',
 
+  server: {
+    port: 3000, // default: 3000
+    host: '0.0.0.0', // default: localhost,
+    timing: false
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'The1-Admin-Portal',
