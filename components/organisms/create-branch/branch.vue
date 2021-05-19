e18fe7bf26f7b22a7836d41be622a054d4bcd323
Wiwat Patanaprasitchai <unknown>
2021-05-19 11:34:37 +0700

    [CHORE] Restore dev

--- components/organisms/create-branch/branch.vue
+++ components/organisms/create-branch/branch.vue
@@ -831,13 +831,7 @@ export default class CreateBranch extends Vue {
   }
 
   brandList: BrandInitialData[] = []
-  partnerCodeList: SiebelPartnerType[] = [
-    {
-      id: 0,
-      partnerCode: "string_mock",
-      partnerName: "string"
-    }
-  ]
+  partnerCodeList: SiebelPartnerType[] = []
   branchTypeList: BranchTypeDataType[] = []
   mallList: MallDataType[] = []
   countryList = [
